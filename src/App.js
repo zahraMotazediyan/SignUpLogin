d8e772d729d84ce7ce6e67607dd878eaec1379a1
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2023-07-07 10:42:01 +0330

    put Route for login and signup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
-// import { Route,Switch } from "react-router-dom";
+import { Route,Switch } from "react-router-dom";
 import './App.css';
-// import SignUp from "./components/SignUp";
+import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 
 function App() {
     return (
         <div className="App">
-            {/*<Switch>*/}
-            {/*    <Route path="/signup" Component={SignUp}/>*/}
-            {/*    <Route path="/login" Component={Login}/>*/}
-            {/*</Switch>*/}
-            <Login/>
+            <Switch>
+                <Route path="/signup" Component={SignUp}/>
+                <Route path="/login" Component={Login}/>
+            </Switch>
         </div>
     );
 }
